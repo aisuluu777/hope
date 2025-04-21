8b286f9df0b62e8add60063c91cc7135190b69f2
aisuluu777 <a30139706@gmail.com>
2025-04-21 13:48:33 +0600

    something

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    task = models.CharField(max_length=45)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.task
+    
